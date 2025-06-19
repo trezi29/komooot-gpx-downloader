21d743c740d0f574f40efc1eba5ad9d3dd55c419
Trezi <trezi29@gmail.com>
2025-06-19 15:28:52 +0200

    fixes build error

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    // remotePatterns: [new URL('https://tourpic-vector.maps.komoot.net/*')],
+    // remotePatterns: [new URL('https://tourpic-vector.maps.komoot.net/**')],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'tourpic-vector.maps.komoot.net',
-        port: '',
-        pathname: '',
-        search: '',
+        pathname: '/**',
       },
     ],
-    domains: ['tourpic-vector.maps.komoot.net'],
   },
 };
 
