8f9b17a2faa2f3008d0c27803d909e68a73d2b74
Trezi <trezi29@gmail.com>
2025-06-20 13:56:18 +0200

    adds ui transition animation

--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,7 @@ export default function Home() {
   const [kmtUrl, setKmtUrl] = useState('');
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const [imgLoaded, setImgLoaded] = useState(false);
 
   function extractIdFromKmtUrl(kmtUrl: string): string | null {
     const match = kmtUrl.match(/tour\/(\d+)/);
@@ -53,6 +54,7 @@ export default function Home() {
 
     if (!response.ok) {
       setError('Failed to fetch tour data');
+      setLoading(false);
       return;
     }
     const tourData = await response.json();
@@ -70,95 +72,128 @@ export default function Home() {
     setLoading(false);
   }
 
+  const animationDuration = 300; // Duration in milliseconds
+
   return (
     <div className="font-ibm p-4 h-screen flex flex-col justify-between items-center">
       <h1 className="font-sora font-bold text-2xl">komoot-gpx-downloader</h1>
-      <div className="p-4 border rounded-md w-full">
-        {!tourData && (
-          <div className="w-full flex flex-col gap-6">
-            <div>
-              <Label htmlFor="kmt-url" className="mb-1.5">
-                Enter komoot’s tour link:
-              </Label>
-              <Input
-                type="text"
-                value={kmtUrl}
-                onChange={(e) => setKmtUrl(e.target.value)}
-                onFocus={() => setError(null)}
-              />
-            </div>
-            <Button
-              disabled={!kmtUrl}
-              onClick={searchTour}
-              className="group self-end relative"
-            >
-              <span className={cn(loading ? 'opacity-0' : 'opacity-100')}>
-                Search Tour
-              </span>
-              <ArrowRight
-                className={cn(
-                  'group-hover:translate-x-1 transition-all duration-150',
-                  loading ? 'opacity-0' : 'opacity-100'
-                )}
-              />
-              {loading && (
-                <LoaderCircle
-                  className="animate-spin absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-                  size={16}
-                />
+      {/* <div className="p-4 border rounded-md w-full max-w-[780px]"> */}
+      <div className="w-screen flex items-center justify-start overflow-hidden">
+        <div
+          className={`flex items-center w-[200vw] transition-transform duration-${animationDuration}`}
+          style={{
+            // opacity: imgLoaded ? '0' : '100%',
+            transform: imgLoaded ? 'translateX(-100vw)' : 'translateX(0)',
+            // display: imgLoaded ? 'none' : 'flex',
+          }}
+        >
+          <div className="w-screen flex justify-center items-center px-4">
+            <div
+              className={cn(
+                'w-full flex flex-col gap-6 p-4 border rounded-md max-w-[780px] transition-opacity duration-150'
               )}
-            </Button>
-          </div>
-        )}
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        {tourData && (
-          <div>
-            <h2 className="font-bold text-lg mb-2">{tourData.name}</h2>
-            <div className="flex flex-col gap-2">
-              <div className="rounded-md h-full min-h-[250px] overflow-hidden flex justify-center items-center">
-                <Image
-                  width={1560}
-                  height={600}
-                  src={tourData.mapImageUrl}
-                  alt="Tour Map Preview"
-                  className="h-full max-w-fit"
-                  // style={{ maxWidth: '100%', height: 'auto' }}
+              style={{ opacity: imgLoaded ? 0 : 1 }}
+            >
+              <div>
+                <Label htmlFor="kmt-url" className="mb-1.5">
+                  Enter komoot’s tour link:
+                </Label>
+                <Input
+                  type="text"
+                  value={kmtUrl}
+                  onChange={(e) => setKmtUrl(e.target.value)}
+                  onFocus={() => setError(null)}
                 />
               </div>
-              <div className="flex justify-between w-full">
-                <div className="flex items-center gap-2">
-                  <MoveHorizontal />
-                  <p>{(tourData.distance / 1000).toFixed(2)} km</p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <MoveUpRightIcon />
-                  <p>{Math.round(tourData.elevation_up)} m</p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <MoveDownRight />
-                  <p>{Math.round(tourData.elevation_down)} m</p>
-                </div>
-              </div>
-            </div>
-            <div className="flex justify-between w-full mt-6">
-              <Button onClick={() => setTourData(null)} variant="outline">
-                <ArrowLeft />
-                Back
-              </Button>
               <Button
-                onClick={() => downloadKomootGpx(tourData.id, tourData.name)}
+                disabled={!kmtUrl}
+                onClick={searchTour}
+                className="group self-end relative"
               >
-                Download GPX
-                <HardDriveDownload />
+                <span className={cn(loading ? 'opacity-0' : 'opacity-100')}>
+                  Search Tour
+                </span>
+                <ArrowRight
+                  className={cn(
+                    'group-hover:translate-x-1 transition-transform duration-150',
+                    loading ? 'opacity-0' : 'opacity-100'
+                  )}
+                />
+                {loading && (
+                  <LoaderCircle
+                    className="animate-spin absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                    size={16}
+                  />
+                )}
               </Button>
+              {error && <p style={{ color: 'red' }}>{error}</p>}
             </div>
           </div>
-        )}
+          <div className="w-screen flex justify-center items-center px-4">
+            {tourData && (
+              <div
+                className="w-full flex flex-col gap-6 transition-opacity duration-150 p-4 border rounded-md max-w-[780px]"
+                style={{ opacity: imgLoaded ? 1 : 0 }}
+              >
+                <h2 className="font-bold text-lg mb-2">{tourData.name}</h2>
+                <div className="flex flex-col gap-2">
+                  <div className="rounded-md h-[300px] overflow-hidden flex justify-center items-center">
+                    <Image
+                      width={1560}
+                      height={600}
+                      src={tourData.mapImageUrl}
+                      alt="Tour Map Preview"
+                      className="h-full max-w-fit"
+                      onLoad={() => setImgLoaded(true)}
+                      // style={{ maxWidth: '100%', height: 'auto' }}
+                    />
+                  </div>
+                  <div className="flex justify-between w-full">
+                    <div className="flex items-center gap-2">
+                      <MoveHorizontal />
+                      <p>{(tourData.distance / 1000).toFixed(2)} km</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <MoveUpRightIcon />
+                      <p>{Math.round(tourData.elevation_up)} m</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <MoveDownRight />
+                      <p>{Math.round(tourData.elevation_down)} m</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-between w-full mt-6">
+                  <Button
+                    onClick={() => {
+                      setImgLoaded(false);
+                      setTimeout(() => {
+                        setTourData(null);
+                      }, animationDuration);
+                    }}
+                    variant="outline"
+                  >
+                    <ArrowLeft />
+                    Back
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      downloadKomootGpx(tourData.id, tourData.name)
+                    }
+                  >
+                    Download GPX
+                    <HardDriveDownload />
+                  </Button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
       <div className="mt-4 flex flex-col gap-2 items-center">
         <p>
           made with ❤️ by{' '}
-          <Link href={'https://wwww.matteotressi.com'} className="underline">
+          <Link href={'https://www.matteotressi.com'} className="underline">
             matteotressi.com
           </Link>
         </p>
